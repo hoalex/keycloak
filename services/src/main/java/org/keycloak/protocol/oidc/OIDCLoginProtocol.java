037e37f20d3323c120b4d303ab744b8e2ae577f3
hoalex <alex@hotz.cc>
2017-05-29 12:12:49 +0200

    Always return session_state as a URL parameter
    The "session_state" URL parameter should always be returned, and not only if the Implicit or Hybrid grant is used.

--- services/src/main/java/org/keycloak/protocol/oidc/OIDCLoginProtocol.java
+++ services/src/main/java/org/keycloak/protocol/oidc/OIDCLoginProtocol.java
@@ -219,12 +219,13 @@ public class OIDCLoginProtocol implements LoginProtocol {
             if (responseType.hasResponseType(OIDCResponseType.TOKEN)) {
                 redirectUri.addParam(OAuth2Constants.ACCESS_TOKEN, res.getToken());
                 redirectUri.addParam("token_type", res.getTokenType());
-                redirectUri.addParam("session_state", res.getSessionState());
                 redirectUri.addParam("expires_in", String.valueOf(res.getExpiresIn()));
             }
 
             redirectUri.addParam("not-before-policy", String.valueOf(res.getNotBeforePolicy()));
         }
+        
+        redirectUri.addParam("session_state", res.getSessionState());
 
         return redirectUri.build();
     }
